1c17b88755108a91078210e5feb6e29488098fd8
MomIsBestFriend <50263213+MomIsBestFriend@users.noreply.github.com>
2020-10-03 15:57:23 +0300

    Setup (#6)
    Co-authored-by: MomIsBestFriend <>

--- setup.py
+++ setup.py
@@ -1,1 +1,10 @@
 #!/usr/bin/env python3
+import re
+
+from setuptools import setup
+
+with open("src/snadra/__init__.py", mode="r", encoding="utf8") as init_file:
+    version = re.search(r'__version__ = "(.*?)"', init_file.read()).group(1)
+
+if __name__ == "__main__":
+    setup(name="snadra", version=version, install_requires=[], extras_require={})

6a2525ec94f8996c501f2db357502999633dad63
MomIsBestFriend <>
2020-10-03 15:10:05 +0300

    BUG: Fix syntax error

--- ci/setup_environment.sh
+++ ci/setup_environment.sh
@@ -6,17 +6,17 @@ echo "Python version"
 python --version
 
 MSG='[Updating pip]' ; echo $MSG
-{ python -m pip install --upgrade pip } &
+python -m pip install --upgrade pip
 wait
 
 MSG='[Updating setuptools]' ; echo $MSG
-{ python -m pip install --upgrade setuptools } &
+python -m pip install --upgrade setuptools
 wait
 
 MSG='[Updating wheel]' ; echo $MSG
-{ python -m pip install --upgrade wheel } &
+python -m pip install --upgrade wheel
 wait
 
 MSG='[Installing dependencies]' ; echo $MSG
-{ python -m pip install --upgrade -r "$BASE_DIR/requirements-dev.txt" } &
+python -m pip install --upgrade -r "$BASE_DIR/requirements-dev.txt"
 wait

a83a60db6ad329bb5c265d26591413bf22202f9a
MomIsBestFriend <>
2020-10-05 13:22:42 +0300

    Check point

--- src/snadra/errors/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Custom exceptions and warnings
-"""
-
-class InsufficientPrivileges(Exception):
-    """
-    Error raised for insufficient privileges of the running user.
-    """
---
+++ src/snadra/snadra.py
@@ -0,0 +1,74 @@
+"""
+foo bar baz
+"""
+import cmd
+import os
+
+from snadra._constants.input import YES, NO
+
+
+class Console(cmd.Cmd):
+    """
+    foo bar baz
+    """
+
+    prompt = "snadra> "
+
+    def __init__(self: "Console"):
+        super().__init__()
+
+    def do_exit(self: "Console", line: str) -> bool:
+        """
+        Exit the shell.
+
+        Parameters
+        ----------
+        self : snadra.Console
+        line: str
+            Arguments that comes after the command, in this case None.
+
+        Returns
+        -------
+        bool
+            True when ever you call this command.
+        """
+        args = line.split(" ")
+
+
+        return True
+
+
+    def do_pwd(self: "Console", line: str) -> None:
+        """
+        Print the current working directory.
+
+        Parameters
+        ----------
+        self: snadra.Console
+        line: str
+            Arguments that comes after the command.
+
+        """
+        current_working_directory = os.getcwd()
+        print(current_working_directory)
+
+    def do_quit(self: "Console", line: str) -> bool:
+        """
+        Exit the shell.
+
+        Parameters
+        ----------
+        self : snadra.Console
+        line: str
+            Arguments that comes after the command, in this case None.
+
+        Returns
+        -------
+        bool
+            True when ever you call this command.
+        """
+        return True
+
+
+if __name__ == "__main__":
+    Console().cmdloop()

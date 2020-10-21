1e1afa4a3adec4cadc7a3f2174f41913cd58672f
Shahar Naveh <50263213+ShaharNaveh@users.noreply.github.com>
2020-10-21 17:28:28 +0300

    Tst cov (#24)
    * Added some test covrage
    
    * Increased test covrage minimum
    
    Co-authored-by: MomIsBestFriend <>

--- src/snadra/commands/exit.py
+++ src/snadra/commands/exit.py
@@ -3,7 +3,7 @@ foo
 """
 from typing import TYPE_CHECKING, Optional
 
-from snadra.commands._base import CommandDefinition
+from snadra.commands._base import CommandDefinition, Complete, Parameter
 
 if TYPE_CHECKING:
     import argparse
@@ -17,5 +17,18 @@ class Command(CommandDefinition):
     KEYWORDS = ["exit", "quit"]
     HELP_TEXT = "Exit the console"
 
+    ARGS = {
+        "-y,--yes": Parameter(
+            complete=Complete.NONE, action="store_true", help="Confirm to exit"
+        )
+    }
+
     def run(self, args: Optional["argparse.Namespace"] = None):
+        """
+        Exit `snadra`.
+        """
+        if args is None or not args.yes:
+            # TODO: Display some message here, to tell the user how to exit
+            return
+
         raise EOFError("Got an exit signal")
--- tests/commands/test_exit.py
+++ tests/commands/test_exit.py
@@ -1,3 +1,5 @@
+import argparse
+
 import pytest
 
 import snadra.commands.exit as module
@@ -5,5 +7,11 @@ import snadra.commands.exit as module
 
 def test_run():
     command = module.Command()
+    assert command.run() is None
+
+
+def test_run_confirm():
+    command = module.Command()
+    args = argparse.Namespace(yes=True)
     with pytest.raises(EOFError, match="Got an exit signal"):
-        command.run()
+        command.run(args)

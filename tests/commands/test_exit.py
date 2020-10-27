95b9e5f1e30617978f828bf8a979f744155393c8
ShaharNaveh <>
2020-10-27 15:10:04 +0200

    Test exit confirm

--- tests/commands/test_exit.py
+++ tests/commands/test_exit.py
@@ -5,13 +5,31 @@ import pytest
 import snadra.commands.exit as module
 
 
-def test_run():
-    command = module.Command()
-    assert command.run() is None
+@pytest.fixture
+def command():
+    """
+    Return the tested command
+    """
+    return module.Command()
+
+class TestExitCommand:
+    def test_confirm(self, capfd, command):
+        expected_err = ""
+        expected_out = "Exit not confirmed"
+
+        command.run()
+
+        captured = capfd.readouterr()
+
+        assert expected_out in captured.out
+        assert expected_err == captured.err
 
 
-def test_run_confirm():
-    command = module.Command()
-    args = argparse.Namespace(yes=True)
-    with pytest.raises(EOFError, match="Got an exit signal"):
-        command.run(args)
+    def test_run(self, command):
+        assert command.run() is None
+
+    def test_run_confirm(self, command):
+        args = argparse.Namespace(yes=True)
+
+        with pytest.raises(EOFError, match="Got an exit signal"):
+            command.run(args)

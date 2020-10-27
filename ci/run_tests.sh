d41a7c611491d5f56103810b9044436b737c391a
ShaharNaveh <>
2020-10-27 13:18:01 +0200

    Added report html for users

--- ci/run_tests.sh
+++ ci/run_tests.sh
@@ -3,13 +3,19 @@
 # Usage:
 # ./ci/run_tests.sh
 
+MINIMUM_TEST_COVRAGE=77.8
 RET_SUM=0
 
 echo "pytest version"
 pytest --version
 
-MSG='Testing'; echo $MSG
-pytest --cov --cov-fail-under=77.8 -v
+MSG='Running tests'; echo $MSG
+if [[ "$GITHUB_ACTIONS" == "true" ]]
+then
+	pytest --cov --cov-fail-under=$MINIMUM_TEST_COVRAGE
+else
+	pytest --cov --cov-fail-under=$MINIMUM_TEST_COVRAGE --cov-report html
+fi
 RET_SUM=$(($RET_SUM + $?))
 
 exit $RET_SUM

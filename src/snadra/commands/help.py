4d03462f6f59daed8c357cf60d6a73dc6313ca3a
ShaharNaveh <>
2020-11-03 15:52:20 +0200

    Add notes

--- src/snadra/commands/help.py
+++ src/snadra/commands/help.py
@@ -11,18 +11,18 @@ from snadra.core.base import CommandDefinition, Complete, Parameter
 if TYPE_CHECKING:
     import argparse
 
+#TODO: Have a function to collect all the keywords
 
 class Command(CommandDefinition):
     """
     The command `help`, for displaying help information about other commands
     """
-
     KEYWORDS = {"help"}
     HELP_TEXT = "List all known commands and print their help message"
     # TODO: Fix this, so the choises will be genereted, and not just debug from 2AM.
     ARGS = {
         "topic": Parameter(
-            Complete.CHOICES, choices={"help", "quit", "exit"}, nargs="?"
+            Complete.CHOICES, choices=_SEE_TODO, nargs="?"
         )
     }
 
@@ -30,4 +30,4 @@ class Command(CommandDefinition):
         """
         foo bar baz
         """
-        snutils.console.log(args, log_locals=True)
+        snutils.console.log(__name__, log_locals=True)

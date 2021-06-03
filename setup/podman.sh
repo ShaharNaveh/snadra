ba0dcc02ef1382804e4d373a5cfd23355977f362
Shahar Naveh <50263213+ShaharNaveh@users.noreply.github.com>
2021-06-03 17:15:25 +0300

    ENH: better integration with podman/docker (#96)
    Co-authored-by: ShaharNaveh <>

---
+++ setup/podman.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+_BASE_DIR="$(dirname $0)/.."
+BASE_DIR=$(realpath $_BASE_DIR)
+
+podman pod create --hostname=snadra_pod --name snadra --publish 5432:5432 # Postgres port
+
+POSTGRES_DATA_DIR="${BASE_DIR}/.data_volume"
+mkdir $POSTGRES_DATA_DIR 2>/dev/null
+
+podman run \
+	--detach \
+	--env POSTGRES_USER=snadraadmin \
+	--env POSTGRES_PASSWORD=snadrapassword \
+	--name=snadra_db \
+	--pod=snadra \
+	--rmi \
+	--volume "${POSTGRES_DATA_DIR}:/var/lib/postgresql/data:Z" \
+	docker.io/postgres:latest
+
+DOCKER_FILE="${BASE_DIR}/Dockerfile"
+
+podman build \
+	--file $DOCKER_FILE \
+	--tag=snadra_app
+
+podman run \
+	--name=snadra_app \
+	--pod=snadra \
+	--rmi \
+	snadra_app

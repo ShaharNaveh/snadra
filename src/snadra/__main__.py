b325ab1dd530f5d2caf8b00adcf6dfa996bb497c
Itay <naveh.itay@gmail.com>
2021-06-05 17:50:09 +0300

    keeping it simple

--- src/snadra/__main__.py
+++ src/snadra/__main__.py
@@ -11,12 +11,9 @@ $ python -m snadra
 """
 import asyncio
 
-from snadra._config import setup_config
 from snadra._core.parsers import CommandParser
 
 if __name__ == "__main__":
-    setup_config()
-
     snadra_console = CommandParser()
     snadra_console._setup_prompt()
     asyncio.run(snadra_console.run())
--- src/snadra/_config/__init__.py
+++ src/snadra/_config/__init__.py
@@ -1,59 +1,11 @@
-"""
-Priority (high to low)
-    - command line arguments
-    - enviorment variables
-    - config file
-    - defaults # TODO
-"""
-
-import getopt
-import os
-from pathlib import Path
-import sys
+import pathlib
 import typing
 
-import toml
-
-config: typing.Dict[str, typing.Optional[str]] = {
-    "postgres_host": None,
-    "postgres_user": None,
-}
+import rtoml
 
 
-def setup_config():
-    config_path = Path(os.environ["HOME"]) / ".config" / "snadra" / "config.toml"
-    # cmd args
+def parse_config_file(path: pathlib.Path) -> typing.Dict[str, typing.Any]:
+    with path.open() as f:
+        config_file_data = rtoml.loads(f.read())
 
-    # TODO:
-    # maybe a list comprehension to get all keys from `config` and add '=' to the end
-    options, _ = getopt.getopt(
-        sys.argv[1:],
-        "c:",
-        [
-            "config=",
-            "postgres_host=",
-            "postgres_user=",
-        ],
-    )
-
-    for option, arg in options:
-        if option in ("-c", "--config"):
-            print(f"config path: {arg}")
-            config_path = Path(arg)
-            continue
-
-        if option[2:] in config.keys():
-            config[option[2:]] = arg
-
-    # env vars + config file
-
-    with config_path.open() as f:
-        config_file_data = toml.loads(f.read())
-
-    for config_name in config.keys():
-        if config[config_name] is None:
-            if (env_name := f"SNADRA_{config_name.upper()}") in os.environ:
-                config[config_name] = os.environ[env_name]
-
-            if config_name in config_file_data:
-                config[config_name] = config_file_data[config_name]
+    return config_file_data

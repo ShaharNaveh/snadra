fb63f56d9f5b48bd7f2c00ff384c0c1476404c39
Shahar Naveh <50263213+ShaharNaveh@users.noreply.github.com>
2021-06-06 17:49:01 +0300

    Update __init__.py

--- src/snadra/_config/__init__.py
+++ src/snadra/_config/__init__.py
@@ -8,4 +8,4 @@ def parse_config_file(path: pathlib.Path) -> typing.Dict:
     with path.open() as f:
         config_file_data = rtoml.loads(f.read())
 
-    return config_file_data
+    return config_file_data  # type: ignore

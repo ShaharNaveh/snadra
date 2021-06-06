5e0e66e6de77186127ff817f0f9942d6e166ea54
Itay Naveh <49409221+ItayNaveh@users.noreply.github.com>
2021-06-06 17:52:03 +0300

    Implement config system partially (#95)
    * Implement config system partially
    without config file and defualts
    
    * implement config file + snake_case
    
    * keeping it simple
    
    * Update __init__.py
    
    * Update __init__.py
    
    Co-authored-by: Shahar Naveh <50263213+ShaharNaveh@users.noreply.github.com>

---
+++ src/snadra/_config/__init__.py
@@ -0,0 +1,11 @@
+import pathlib
+import typing
+
+import rtoml
+
+
+def parse_config_file(path: pathlib.Path) -> typing.Dict:
+    with path.open() as f:
+        config_file_data = rtoml.loads(f.read())
+
+    return config_file_data  # type: ignore

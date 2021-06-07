43b7002612f98bc42999b38baeabbe05d7376c6c
ShaharNaveh <>
2021-06-07 13:34:32 +0300

    Fix lint issue

--- src/snadra/_config/__init__.py
+++ src/snadra/_config/__init__.py
@@ -1,10 +1,12 @@
-import pathlib
-import typing
+from typing import TYPE_CHECKING
 
 import rtoml
 
+if TYPE_CHECKING:
+    import pathlib
 
-def parse_config_file(path: pathlib.Path):
+
+def parse_config_file(path: "pathlib.Path"):
     with path.open() as f:
         config_file_data = rtoml.loads(f.read())
 

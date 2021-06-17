677117681fa9a50ba01020ac68a645bdc01e1f1e
ShaharNaveh <>
2021-06-17 10:45:09 +0300

    CLN: Removed unused code

--- src/_snadra/cmd/__init__.py
+++ src/_snadra/cmd/__init__.py
@@ -1,10 +1,9 @@
-from _snadra.cmd.base import CommandDefinition, Complete
+from _snadra.cmd.base import Complete
 from _snadra.cmd.console import SnadraConsole
 from _snadra.cmd.parsers import CommandParser, Commands
 from _snadra.cmd.utils import CommandMeta
 
 __all__ = [
-    "CommandDefinition",
     "CommandMeta",
     "CommandParser",
     "Commands",

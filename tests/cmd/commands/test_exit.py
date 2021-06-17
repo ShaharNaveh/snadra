93f1c6bf9077efb6f63a28eda8ac863af77a4634
ShaharNaveh <>
2021-06-17 12:17:17 +0300

    Fixed tests

--- tests/cmd/commands/test_exit.py
+++ tests/cmd/commands/test_exit.py
@@ -15,24 +15,8 @@ def command():
 
 @pytest.mark.asyncio
 class TestExitCommand:
-    async def test_confirm(self, capfd, command):
-        args = argparse.Namespace(yes=False)
-        expected_err = ""
-        expected_out = "Exit not confirmed"
-
-        await command.run(args)
-
-        captured = capfd.readouterr()
-
-        assert expected_out in captured.out
-        assert expected_err == captured.err
-
     async def test_run(self, command):
-        args = argparse.Namespace(yes=False)
-        assert await command.run(args) is None
-
-    async def test_run_confirm(self, command):
-        args = argparse.Namespace(yes=True)
+        args = argparse.Namespace()
 
         with pytest.raises(EOFError, match="Got an exit signal"):
             await command.run(args)

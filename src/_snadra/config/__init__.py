d8f3d595e349e35061723744bfaf41b30b96d8a9
ShaharNaveh <>
2021-06-19 19:31:38 +0300

    CLN: Improved config

--- src/_snadra/config/__init__.py
+++ src/_snadra/config/__init__.py
@@ -1,3 +1,8 @@
-from _snadra.config.utils import config_file_location, parse_config_file
+from _snadra.config.constants import DEFAULT_CONFIG, DEFAULT_CONFIG_LOCATION
+from _snadra.config.utils import parse_config_file
 
-__all__ = ["config_file_location", "parse_config_file"]
+__all__ = [
+    "DEFAULT_CONFIG",
+    "DEFAULT_LOCATION",
+    "parse_config_file",
+]

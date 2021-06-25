79f6620579d0581745e940c7a8d0609b99583288
Shahar Naveh <50263213+ShaharNaveh@users.noreply.github.com>
2021-06-25 13:10:05 +0300

    TST: add tests (#115)
    * CLN: Removed "Commands" from __init__.py
    
    * Fix failing tests
    
    * Added pytest-cov
    
    * TST: Added tests
    
    * CLN: Removed unused "type: ignore"
    
    Co-authored-by: ShaharNaveh <>

--- src/_snadra/cmd/__init__.py
+++ src/_snadra/cmd/__init__.py
@@ -1,4 +1,4 @@
-from _snadra.cmd.commands import Commands
+from _snadra.cmd.base import Commands
 from _snadra.cmd.utils import CommandMeta
 
 __all__ = [
---
+++ tests/cmd/test_utils.py
@@ -0,0 +1,23 @@
+from _snadra.cmd.utils import iter_dir
+
+
+def test_iter_dir(modules_dir):
+    result = [path.name for path in iter_dir(modules_dir)]
+    expected = ["module_1.py", "module_2.py"]
+    assert sorted(result) == sorted(expected)
+
+
+def test_iter_dir_skip(modules_dir):
+    skip = {"module_2"}
+    result = [path.name for path in iter_dir(modules_dir, skip=skip)]
+    expected = ["module_1.py"]
+    assert sorted(result) == sorted(expected)
+
+
+def test_iter_dir_include_suffix(modules_dir):
+    include_suffixes = {".txt"}
+    result = [
+        path.name for path in iter_dir(modules_dir, include_suffixes=include_suffixes)
+    ]
+    expected = ["module_4.txt"]
+    assert sorted(result) == sorted(expected)

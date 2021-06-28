e973a0f9b57443061c116ebb016120894522d43b
Shahar Naveh <50263213+ShaharNaveh@users.noreply.github.com>
2021-06-28 20:45:04 +0300

    ENH: add workspace (#116)
    * Basic add
    
    * Fix merge grabage
    
    * Base skeleton
    
    * Base working add and delete of workspaces
    
    * Make workspace table to not connect to the db
    
    * Added cache for workspace table preview

--- src/_snadra/cmd/commands/workspace.py
+++ src/_snadra/cmd/commands/workspace.py
@@ -1,19 +1,25 @@
 """
 Manage workspaces.
 """
+import functools
 from typing import TYPE_CHECKING
 
 from rich import box as rich_box
 from rich.table import Table as RichTable
+from sqlalchemy import delete
 from sqlalchemy.future import select
 
 from _snadra.cmd import CommandMeta
 from _snadra.cmd.utils import console
 from _snadra.db.config import async_session
 from _snadra.db.models import Workspace
+from _snadra.state import state
 
 if TYPE_CHECKING:
     import argparse
+
+    from sqlalchemy.engine.result import ChunkedIteratorResult
+    from sqlalchemy.ext.asyncio import AsyncSession
 
 
 class Command(CommandMeta):
@@ -27,32 +33,62 @@ class Command(CommandMeta):
     long_help = "LONG HELP FOR WORKSPACE COMMAND"
 
     arguments = {
-        "-a,--add": {"help": "Add a workspace", "action": "store_true"},
-        "-d,--delete": {"help": "Delete a workspace", "action": "store_true"},
-        "-s,--search": {"help": "Search a workspace"},
+        "target": {"help": "Target workspace", "metavar": "target", "nargs": "?"},
+        "-a,--add": {"action": "store_true", "help": "Add a workspace"},
+        "-d,--delete": {"action": "store_true", "help": "Delete a workspace"},
+        "--desc,--description": {"help": "Description for the workspace"},
     }
 
-    async def run(self, args: "argparse.Namespace") -> None:
-        """
-        Manage workspaces.
+    @staticmethod
+    async def is_workspace_exists(
+        target: str, *, async_session: "AsyncSession"
+    ) -> bool:
+        async with async_session() as session:
+            async with session.begin():
+                stmt = select(Workspace).where(Workspace.name == target)
+                result = await session.execute(stmt)
+        workspace = result.scalar_one_or_none()
 
-        Parameters
-        ----------
-        args : :class:`argparse.Namespace`
-            The arguments for the command.
-        """
-        workspace_display_table = RichTable(title="Workspaces", box=rich_box.SIMPLE)
-        workspace_display_table.add_column("Name")
-        workspace_display_table.add_column("Description")
-        workspace_display_table.add_column("Created at")
-        workspace_display_table.add_column("Updated at")
+        return bool(workspace)
 
+    @staticmethod
+    async def add_workspace(
+        target: str, desc: str, *, async_session: "AsyncSession"
+    ) -> None:
+        async with async_session() as session:
+            async with session.begin():
+                workspace = Workspace(name=target, description=desc)
+                session.add(workspace)
+                await session.commit()
+
+    @staticmethod
+    async def delete_workspace(target: str, *, async_session: "AsyncSession") -> None:
+        async with async_session() as session:
+            async with session.begin():
+                stmt = delete(Workspace).where(Workspace.name == target)
+                await session.execute(stmt)
+
+    @staticmethod
+    async def all_workspaces(
+        *, async_session: "AsyncSession"
+    ) -> "ChunkedIteratorResult":
         async with async_session() as session:
             async with session.begin():
                 stmt = select(Workspace)
                 result = await session.execute(stmt)
 
-        for workspace_metadata in result.scalars():
+        return result
+
+    @staticmethod
+    @functools.lru_cache
+    def workspace_table(workspaces: "ChunkedIteratorResult") -> RichTable:
+        table = RichTable(title="Workspaces", box=rich_box.SIMPLE)
+        table.add_column("Name")
+        table.add_column("Description")
+        table.add_column("Created at")
+        table.add_column("Updated at")
+
+        for workspace_metadata in workspaces.scalars():
             name = workspace_metadata.name
             description = workspace_metadata.description
             created_at = workspace_metadata.created_at.strftime("%d/%m/%Y, %H:%M:%S")
@@ -63,6 +99,67 @@ class Command(CommandMeta):
             else:
                 updated_at = updated_at.strftime("%d/%m/%Y, %H:%M:%S")
 
-            workspace_display_table.add_row(name, description, created_at, updated_at)
+            table.add_row(name, description, created_at, updated_at)
 
-        console.print(workspace_display_table)
+        return table
+
+    async def run(self, args: "argparse.Namespace") -> None:
+        """
+        Manage workspaces.
+
+        Parameters
+        ----------
+        args : :class:`argparse.Namespace`
+            The arguments for the command.
+        """
+        target = args.target
+        do_add = args.add
+        do_delete = args.delete
+
+        if do_add and do_delete:
+            # Error: conflicting flags
+            console.log("[red]Error[/red]: Conflicting flags 'add' and 'delete'")
+
+        if target is not None:
+            is_exists = await self.is_workspace_exists(
+                target=target, async_session=async_session
+            )
+            if do_add:
+                # Add workspace
+                if is_exists:
+                    console.log("Workspace already exists!")
+                    return
+                await Command.add_workspace(
+                    target=target, desc=args.desc, async_session=async_session
+                )
+            elif do_delete:
+                # Delete workspace
+                if not is_exists:
+                    console.log(
+                        f"[red]Error[/red]: Workspace {repr(target)} does not exists!"
+                    )
+                    return
+                await Command.delete_workspace(
+                    target=target, async_session=async_session
+                )
+            else:
+                # Switch to workspace
+                if not is_exists:
+                    console.log(
+                        f"[red]Error[/red]: Workspace {repr(target)} does not exists!"
+                    )
+                    return
+                state["current_workspace"] = target
+        else:
+            if do_add:
+                # Error: missing argument
+                console.log("Missing argument 'target'")
+            elif do_delete:
+                # Error: missing argument
+                console.log("Missing argument 'target'")
+            else:
+                # Show workspaces
+                workspaces = await Command.all_workspaces(async_session=async_session)
+
+                table = Command.workspace_table(workspaces=workspaces)
+                console.print(table)
--- src/_snadra/db/models/workspace.py
+++ src/_snadra/db/models/workspace.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, DateTime, Integer, Sequence, String, Text
+from sqlalchemy import Column, DateTime, String, Text
 from sqlalchemy.sql import func
 
 from _snadra.db.config import Base
@@ -7,8 +7,7 @@ from _snadra.db.config import Base
 class Workspace(Base):  # type: ignore
     __tablename__ = "workspaces"
 
-    id = Column(Integer, Sequence("workspace_id_seq"), primary_key=True, nullable=False)
-    name = Column(Text, nullable=False)
+    name = Column(Text, primary_key=True, nullable=False)
     description = Column(String(length=4096), nullable=False, server_default="")
     created_at = Column(
         DateTime(timezone=True), server_default=func.now(), nullable=False
